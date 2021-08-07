884f321ee62605a43cf06399b8c36defb31b88d8
mghoneem10 <mghoneem10@gmail.com>
2021-08-07 18:45:12 +0200

    Commission Payment

--- nano/nano/doctype/commission_payment/commission_payment.js
+++ nano/nano/doctype/commission_payment/commission_payment.js
@@ -1,8 +1,24 @@
 // Copyright (c) 2021, ERP Cloud Systems and contributors
 // For license information, please see license.txt
 
-frappe.ui.form.on('Commission Payment', {
-	// refresh: function(frm) {
+frappe.ui.form.on('Commission Payment',  'validate',  function(frm, cdt, cdn) {
+    if(frm.doc.__islocal ? 0 : 1){
+         var dw = locals[cdt][cdn];
+        var total = 0;
+        frm.doc.commission_details.forEach(function(dw) { total += dw.commissions; });
+        frm.set_value("total_payable", total);
+        refresh_field("total_payable");
+    }
+});
 
-	// }
+   frappe.ui.form.on("Commission Payment", {
+    setup: function(frm) {
+		frm.set_query("sales_manager", function() {
+			return {
+				filters: [
+					["Sales Partner", "name", "in", ["Zainab Farouk","Khalid Matter"]]
+				]
+			};
+		});
+	}
 });
